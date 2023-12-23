59ca0ef90571b756456a75642d9e72e1c209c32f
Cooper Saye <coopersaye@gmail.com>
2023-12-23 10:26:39 -0800

    Cool text effect

--- components/Globe/index.tsx
+++ components/Globe/index.tsx
@@ -1,100 +1,49 @@
 import styles from './styles.module.scss'
-import { Canvas, useThree, useFrame } from '@react-three/fiber'
-import { useRef, useLayoutEffect, Fragment, RefObject } from 'react'
-import {
-  useTransform,
-  useScroll,
-  useTime,
-  motion,
-  easeIn,
-  easeOut,
-  easeInOut,
-} from 'framer-motion'
-import { degreesToRadians, progress, mix } from 'popmotion'
-import { Mesh } from 'three'
-import { useWindowResize } from '@/hooks/useWindowResize'
-import { Text } from '@react-three/drei'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useTransform, useScroll, motion, easeOut } from 'framer-motion'
+import { degreesToRadians } from 'popmotion'
+import { Center, Text3D } from '@react-three/drei'
 
 const color = '#eeeeee'
 
-const scrollRange = [0, 0.5]
+const scrollRange = [0, 0.2]
 
 function HelloWorldText() {
   return (
-    <Text
-      color={color}
-      position={[0, 0, 0]}
-      fontSize={0.6}
-      font='/fonts/Inter/Inter-Bold.ttf'
-    >
-      COOPER SAYE
-    </Text>
+    <Center>
+      <Text3D font='/fonts/Inter/Inter_Bold.json' position={[0, 0, 0]}>
+        Cooper Saye
+        <meshBasicMaterial wireframe color={color} />
+      </Text3D>
+    </Center>
   )
 }
 
-function Star({ p }: { p: number }) {
-  const ref = useRef<Mesh>(null)
-
-  useLayoutEffect(() => {
-    const distance = mix(2.5, 3, Math.random())
-    const yAngle = mix(
-      degreesToRadians(15),
-      degreesToRadians(165),
-      Math.random()
-    )
-    const xAngle = degreesToRadians(360) * p
-    ref.current!.position.setFromSphericalCoords(distance, yAngle, xAngle)
-  })
-
-  return (
-    <mesh ref={ref}>
-      <boxGeometry args={[0.05, 0.05, 0.05]} />
-      <meshBasicMaterial
-        wireframe
-        color={color}
-        transparent={true}
-        opacity={0.1}
-      />
-    </mesh>
-  )
-}
-
-function Scene({ numStars = 200 }) {
-  const gl = useThree((state) => state.gl)
+function Scene() {
   const { scrollYProgress } = useScroll()
+  const xAngle = useTransform(scrollYProgress, scrollRange, [
+    degreesToRadians(0),
+    degreesToRadians(540),
+  ])
   const yAngle = useTransform(scrollYProgress, scrollRange, [
     degreesToRadians(90),
-    degreesToRadians(180),
+    degreesToRadians(0),
   ])
-  const distance = useTransform(scrollYProgress, scrollRange, [5.5, 30], {
+  const distance = useTransform(scrollYProgress, scrollRange, [5, 30], {
     ease: easeOut,
   })
-  const time = useTime()
 
   useFrame(({ camera }) => {
     camera.position.setFromSphericalCoords(
       distance.get(),
       yAngle.get(),
-      time.get() * 0.0003
+      xAngle.get()
     )
     camera.updateProjectionMatrix()
     camera.lookAt(0, 0, 0)
   })
 
-  useLayoutEffect(() => gl.setPixelRatio(0.3))
-  useWindowResize(() => gl.setPixelRatio(0.3))
-
-  const stars = []
-  for (let i = 0; i < numStars; i++) {
-    stars.push(<Star p={progress(0, numStars, i)} />)
-  }
-
-  return (
-    <Fragment>
-      {stars}
-      <HelloWorldText />
-    </Fragment>
-  )
+  return <HelloWorldText />
 }
 
 export function Globe() {
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,24 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 import styles from '@/styles/pages/Index.module.scss'
 import { Globe } from '@/components/Globe'
 
-const scrollRange = [0.4, 0.8]
-
 export default function Index() {
   const { scrollYProgress } = useScroll()
-  const opacity = useTransform(scrollYProgress, scrollRange, [0, 1])
-  const scale = useTransform(scrollYProgress, scrollRange, [1, 3])
+  const background = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.6, 1],
+    ['#111111', '#e7f055', '#356bf6', '#0b2749', '#eeeeee']
+  )
 
   return (
     <div className={styles.container}>
-      <div className={styles.globeContainer}>
+      <motion.div className={styles.background} style={{ background }} />
+      <div className={styles.heroSection}>
         <Globe />
       </div>
-      <div className={styles.bodyContainer}>
-        <motion.div
-          className={styles.circle}
-          style={{ opacity, scale }}
-        ></motion.div>
-      </div>
+      <div className={styles.rampSection}></div>
+      <div className={styles.contrarySection}></div>
+      <div className={styles.michiganSection}></div>
+      <div className={styles.footerSection}></div>
     </div>
   )
 }
