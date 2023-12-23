4c7c6f6126a1c996833ce2d859af6bd0903adc48
Cooper Saye <coopersaye@gmail.com>
2023-12-23 00:56:28 -0800

    Cool animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,10 @@
-import { motion } from 'framer-motion'
 import styles from '@/styles/pages/Index.module.scss'
+import { Globe } from '@/components/Globe'
 
 export default function Index() {
   return (
     <div className={styles.container}>
-      <div className={styles.hero}>
-        <motion.div
-          className={styles.box}
-          animate={{
-            scale: [1, 2, 2, 1, 1],
-            rotate: [0, 0, 270, 270, 0],
-            borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-          }}
-          transition={{
-            duration: 2,
-            ease: 'easeInOut',
-            times: [0, 0.2, 0.5, 0.8, 1],
-            repeat: Infinity,
-            repeatDelay: 1,
-          }}
-        />
-      </div>
-      <div className={styles.body}>
-        <p>Welcome to the body</p>
-      </div>
+      <Globe />
     </div>
   )
 }
